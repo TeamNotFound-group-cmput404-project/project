44aee162d3b189316f0a089cac3be53f4ce0b67f
Shway <shuwei4@ualberta.ca>
2021-02-18 19:29:36 -0700

    test

--- Iconicity/admin.py
+++ Iconicity/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import *
+
 # Register your models here.
-
-"""
-https://docs.djangoproject.com/en/3.1/topics/serialization/#:~:text=Django's%20serialization%20framework%20provides%20a,See%20also
-
-"""
-class profileAdmin(admin.ModelAdmin):
-
