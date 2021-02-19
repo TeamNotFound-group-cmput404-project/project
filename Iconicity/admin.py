6a8c538b8bed8c0b06dcd7d3592be8505443dbc6
Qianxi Li <45400887+liqianxi@users.noreply.github.com>
2021-02-18 23:40:12 -0800

    Update admin.py

--- Iconicity/admin.py
+++ Iconicity/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
-admin.site.register(Inbox)
+admin.site.register(UserProfile)
 admin.site.register(LikeSingle)
 admin.site.register(Comment)
 admin.site.register(FriendRequest)
 admin.site.register(Post)
-admin.site.register(UserProfile)
 
+admin.site.register(Inbox)
