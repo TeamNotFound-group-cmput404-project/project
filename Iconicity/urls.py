84536e876e17ca7c431c94225ae61dee85414b0b
Qianxi Li <qianxi@ualberta.ca>
2021-02-24 11:22:35 -0800

    finish get current user profile and display it on the main_page

--- Iconicity/urls.py
+++ Iconicity/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-	#path('', views.login, name = 'login'),
 	path('', views.LoginView.as_view(), name = 'login'),
 	path('logout', views.logout_view, name = 'logout'),
 	path('signup',views.signup,name = 'signup'),
 	path('main', views.main_page, name = 'main_page'),
+	#path('author', views.getUserProfile, name = 'userprofile')
 ]
