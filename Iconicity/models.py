51c5addd8cbea9ae1db87d6f5a677d67c6c7e2d1
Peter Zhou <zhiqi3@ualberta.ca>
2021-02-26 21:58:56 -0700

    create post

--- Iconicity/models.py
+++ Iconicity/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+from django.forms.models import model_to_dict
 # Create your models here.
 
 """Reference (move to other locations later)
@@ -118,22 +120,17 @@ class Post(models.Model):
 
     # https://stackoverflow.com/questions/15874233/output-django-queryset-as-json
     def as_dict(self):
-        return {
-            "post_id":post_id,
-            "title":title,
-            "type":type,
-            "source":source,
-            "origin":origin,
-            "description":description,
-            "contentType":contentType,
-            "content":content,
-            "author":author,
-            "categories":categories,
-            #"count":count,
-            "comments":comments,
-            "published":published,
-            "visibility":visibility,
-            "unlisted":unlisted }
+        values = []
+        temp = Post.objects.values()
+        for value in temp:
+            print(type(value))
+            value["post_id"] = str(value["post_id"])
+            value["author_id"] = str(value["author_id"])
+            values.append(value)
+        return values
+    
+    def get_absolute_url(self):
+        return reverse("main_page")
 
 
 class FriendRequest(models.Model):
