c72e366c2f1aebcfa86c55d48d7d5d98f6006a0e
Qianxi Li <qianxi@ualberta.ca>
2021-03-03 22:26:20 -0800

    fix indentation

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 
-    django_on_heroku.settings(locals()) # bottom of the file
+django_on_heroku.settings(locals()) # bottom of the file
