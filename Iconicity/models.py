b88ebfe3929127cffb4bc6ca406167b8e8f9860c
Peter Zhou <zhiqi3@ualberta.ca>
2021-03-04 17:29:02 -0700

    Revert "display comment author and body"
    This reverts commit 5b1591b2d8071bffd3fd5e69eaa6287bd1b035a1.

--- Iconicity/models.py
+++ Iconicity/models.py
@@ -213,7 +213,7 @@ class FriendRequest(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     type = models.CharField(max_length=10, default="comment")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     # ISO 8601 TIMESTAMP
     # publish time
     published = models.DateTimeField(default=timezone.now)
@@ -231,7 +231,7 @@ class Comment(models.Model):
                                    default="")
 
     def __str__(self):
-        return '%s' % (self.author)
+        return '%s - %s - %s' % (self.post.title, self.author, self.id)
 
 class LikeSingle(models.Model):
     """
