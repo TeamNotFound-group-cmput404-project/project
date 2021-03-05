0ed11a7c80686db566fecdd8e36042ac7c0f54f5
Shway <shuwei4@ualberta.ca>
2021-03-04 22:56:44 -0700

    test

--- Iconicity/views.py
+++ Iconicity/views.py
@@ -303,6 +303,8 @@ def follow_someone(request):
         curProfile = UserProfile.objects.get(user = request.user)
         # save the new uid into current user's follow:
         curProfile.follow.add(followee_profile.user)
+        # for external uses:
+        curProfile.externalFollow['urls'].append(followee_profile.host)
         curProfile.save()
         # stay on the same page
         return redirect(request.META.get('HTTP_REFERER'))
@@ -315,6 +317,8 @@ def unfollow_someone(request):
         curProfile = UserProfile.objects.get(user = request.user)
         # remove the uid from current user's follow:
         curProfile.follow.remove(followee_profile.user)
+        # for external uses:
+        curProfile.externalFollow['urls'].remove(followee_profile.host)
         curProfile.save()
         # stay on the same page
         return redirect(request.META.get('HTTP_REFERER'))
@@ -341,7 +345,9 @@ def accept_friend_request(request):
         receiver = UserProfile.objects.get(user = request.user)
         # save the new friend's uid into current user's follow and vice versa:
         sender.follow.add(receiver.user)
+        sender.externalFollow['urls'].append(receiver.host) # external connectivity
         receiver.follow.add(sender.user)
+        receiver.externalFollow['urls'].append(sender.host) # external connectivity
         sender.save()
         receiver.save()
         # change the status of the friend request to accepted:
@@ -452,7 +458,9 @@ def pre_delete_remove_from_follow(sender, instance, **kwargs):
     sender = instance.actor
     receiver = instance.object_author
     sender.follow.remove(receiver.user)
+    sender.externalFollow['urls'].remove(receiver.host) # external connectivity
     receiver.follow.remove(sender.user)
+    receiver.externalFollow['urls'].remove(sender.host) # external connectivity
     sender.save()
     receiver.save()
 
