b04a793a399da2b0063c6ec509f7594bf6599244
Shway <shuwei4@ualberta.ca>
2021-03-23 20:12:52 -0600

    test

--- Iconicity/views.py
+++ Iconicity/views.py
@@ -366,7 +366,6 @@ class UserProfileListView(ListView):
         # get all profiles except for current user from both local host and remote hosts:
         local = UserProfile.objects.get_all_profiles(exception = self.request.user)
         remote = getAllExternalAuthors()
-        print(remote)
         return remote + list(local)
     # override:
     def get_context_data(self, **kwargs):
