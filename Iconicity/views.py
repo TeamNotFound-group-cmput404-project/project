28ae679c079e06ae2c4db81c680514b16a309a07
Shway <shuwei4@ualberta.ca>
2021-03-24 16:27:02 -0600

    test

--- Iconicity/views.py
+++ Iconicity/views.py
@@ -797,74 +797,80 @@ class Inboxs(APIView):
 
     def post(self, request, author_id):
         data_json = request.data
+        print("data received at inbox: "data_json)
         local_author_profile = UserProfile.objects.get(pk=author_id)
-        inbox_obj = Inbox.objects.get(author=local_author_profile.url)
-        if data_json['type'] == "Like":
-            post_url = data_json["object"]
-            post_id = [i for i in post_url.split('/') if i][-1]
-            post_obj = Post.objects.get(pk=post_id)
-            print("post_url",post_url)
-            print("host",request.META['HTTP_HOST'])
-            if request.META['HTTP_HOST'] in data_json['author']['url']:
-                # means it's local like author
-                if local_author_profile.user in post_obj.like:
-                    # means this man liked the post before
-                    # we should make him unlike this post
-                    post_obj.like.remove(local_author_profile.user)
-                    post_obj.save()
-                    return Response(InboxSerializer(inbox_obj).data,status=204)
+        print(local_author_profile.url)
+        try:
+            inbox_obj = Inbox.objects.get(author=local_author_profile.url)
+            if data_json['type'] == "Like":
+                # if the type is “like” then add that like to the author’s inbox
+                post_url = data_json["object"]
+                post_id = [i for i in post_url.split('/') if i][-1]
+                post_obj = Post.objects.get(pk=post_id)
+                print("post_url",post_url)
+                print("host",request.META['HTTP_HOST'])
+                if request.META['HTTP_HOST'] in data_json['author']['url']:
+                    # means it's local like author
+                    if local_author_profile.user in post_obj.like:
+                        # means this man liked the post before
+                        # we should make him unlike this post
+                        post_obj.like.remove(local_author_profile.user)
+                        post_obj.save()
+                        return Response(InboxSerializer(inbox_obj).data,status=204)
+                    else:
+                        # means add this man's id to the like list.
+                        post_obj.like.append(local_author_profile.user)
+                        like_obj = Like()
+                        like_obj.context= data_json["context"]
+                        like_obj.object = data_json["object"]
+                        like_obj.summary = data_json["summary"]
+                        like_obj.author = data_json["author"]
+                        like_obj.save()
+                        post_obj.save()
+                        like_json = LikeSerializer(like_obj).data
+                        inbox_obj.items['Like'].append(like_json)
+                        inbox_obj.save()
+                        return Response(InboxSerializer(inbox_obj).data,status=201)
                 else:
-                    # means add this man's id to the like list.
-                    post_obj.like.append(local_author_profile.user)
-                    like_obj = Like()
-                    like_obj.context= data_json["context"]
-                    like_obj.object = data_json["object"]
-                    like_obj.summary = data_json["summary"]
-                    like_obj.author = data_json["author"]
-                    like_obj.save()
-                    post_obj.save()
-                    like_json = LikeSerializer(like_obj).data
-                    inbox_obj.items['Like'].append(like_json)
-                    inbox_obj.save()
-                    return Response(InboxSerializer(inbox_obj).data,status=201)
+                    # means it's a external author
+                    external_author_url = data_json["author"]["url"]
+                    print("external likes",post_obj.external_likes)
+                    if post_obj.external_likes == {} or post_obj.external_likes == {"urls":[]} or data_json["author"]["url"] not in post_obj.external_likes['urls']:
+                        # means add this man's id to the external like list.
+                        post_obj.external_likes['urls'] = []
+                        post_obj.external_likes['urls'].append(external_author_url)
+                        like_obj = Like()
+                        like_obj.context= data_json["context"]
+                        like_obj.object = data_json["object"]
+                        like_obj.summary = data_json["summary"]
+                        like_obj.author = data_json["author"]
+                        like_obj.save()
+                        post_obj.save()
+                        like_json = LikeSerializer(like_obj).data
+                        inbox_obj.items['Like'].append(like_json)
+                        inbox_obj.save()
+                        return Response(InboxSerializer(inbox_obj).data,status=201)
+                    else:
+                        # means this man liked the post before
+                        # we should make him unlike this post
+                        post_obj.external_likes['urls'].remove(external_author_url)
+                        post_obj.save()
+                        return Response(InboxSerializer(inbox_obj).data,status=204)
+            
+            elif (data_json['type'] == "post" or data_json['type'] == "Post"):
+                # if the type is “post” then add that post to the author’s inbox
+                # add a post to the author_id's inbox
+                inbox_obj.items['Post'].append(data_json)
+                return Response(InboxSerializer(inbox_obj).data,status=200)
+
+            elif data_json['type'] == "Follow": 
+                # if the type is “follow” then add that follow is added to the author’s inbox to approve later   
+                inbox_obj.items['Follow'].append(data_json)
+                return Response(InboxSerializer(inbox_obj).data,status=200)
             else:
-                # means it's a external author
-                external_author_url = data_json["author"]["url"]
-                print("external likes",post_obj.external_likes)
-                if post_obj.external_likes == {} or post_obj.external_likes == {"urls":[]} or data_json["author"]["url"] not in post_obj.external_likes['urls']:
-                    # means add this man's id to the external like list.
-                    post_obj.external_likes['urls'] = []
-                    post_obj.external_likes['urls'].append(external_author_url)
-                    like_obj = Like()
-                    like_obj.context= data_json["context"]
-                    like_obj.object = data_json["object"]
-                    like_obj.summary = data_json["summary"]
-                    like_obj.author = data_json["author"]
-                    like_obj.save()
-                    post_obj.save()
-                    like_json = LikeSerializer(like_obj).data
-                    inbox_obj.items['Like'].append(like_json)
-                    inbox_obj.save()
-                    return Response(InboxSerializer(inbox_obj).data,status=201)
-                else:
-                    # means this man liked the post before
-                    # we should make him unlike this post
-                    post_obj.external_likes['urls'].remove(external_author_url)
-                    post_obj.save()
-                    return Response(InboxSerializer(inbox_obj).data,status=204)
-
-        elif (data_json['type'] == "post" or data_json['type'] == "Post"):
-            # add a post to the author_id's inbox
-            inbox_obj.items['Post'].append(data_json)
-            return Response(InboxSerializer(inbox_obj).data,status=200)
-
-        elif data_json['type'] == "Follow":    
-            inbox_obj.items['Follow'].append(data_json)
-            return Response(InboxSerializer(inbox_obj).data,status=200)    
-
-        # if the type is “post” then add that post to the author’s inbox
-        # if the type is “follow” then add that follow is added to the author’s inbox to approve later
-        # if the type is “like” then add that like to the author’s inbox
+                print("Inbox operation not handled")
+        except Exception as e:
+            print(e)
 
     def delete(self, request, author_id):
         # clear the inbox
