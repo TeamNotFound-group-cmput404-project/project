20f2f729b2928824ad1b98675532055bd36b8c2a
Ubuntu <ubuntu@ubuntu2010.linuxvmimages.local>
2021-03-31 07:04:25 -0400

    test

--- Iconicity/views.py
+++ Iconicity/views.py
@@ -1094,7 +1094,7 @@ class Inboxs(APIView):
         return Response(InboxSerializer(inbox).data)
 
     def post(self, request, author_id):
-        data_json = json.loads(request.data)
+        data_json = request.data
         print("data_json", data_json)
         local_author_profile = UserProfile.objects.get(pk=author_id)
         try:
