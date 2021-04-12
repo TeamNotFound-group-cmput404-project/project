6e1774046be8d5c542795f6ea73a2975c61bf37a
Meilin-Lyu <mlyu@ualberta.ca>
2021-04-12 00:15:18 -0600

    test

---
+++ Iconicity/tests/test_url.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from whatever.models import Whatever
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from whatever.forms import WhateverForm
+
+# models test
+class WhateverTest(TestCase):
+    def create_whatever(self, title="only a test", body="yes, this is only a test"):
+        return Whatever.objects.create(title=title, body=body, created_at=timezone.now())
+
+    def test_whatever_creation(self):
+        w = self.create_whatever()
+        self.assertTrue(isinstance(w, Whatever))
+        self.assertEqual(w.__unicode__(), w.title)
