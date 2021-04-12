b7eb66354a737805dc8d507d9a6fbd6277b7b261
Ubuntu <ubuntu@ubuntu2010.linuxvmimages.local>
2021-04-11 20:31:14 -0400

    test

--- Iconicity/views.py
+++ Iconicity/views.py
@@ -1654,6 +1654,7 @@ class FriendPostsByAuthor(APIView):
 
 def post_comments(request):
     ppid = request.POST.get('ppid')
+    if ppid is None: ppid = request.POST.get('pk')
     if ppid:
         
         context = {'form123': CommentsCreateForm(), "url": ppid}
