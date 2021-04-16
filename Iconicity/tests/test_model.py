0bcb8b5315395a666b9cb24af726c2e6506fb2dd
Meilin-Lyu <mlyu@ualberta.ca>
2021-04-16 10:20:55 -0600

    add model testcase

--- Iconicity/tests/test_model.py
+++ Iconicity/tests/test_model.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from Iconicity.models import Post,FriendRequest,UserProfile
+from Iconicity.models import Post,FriendRequest,UserProfile,Comment
 from django.contrib.auth.models import User
 from .test_setup import TestSetUp
 
@@ -38,9 +38,43 @@ class TestModels(TestSetUp):
 
 
     def test_model_user_profile(self):
-        User.objects.create(id = 2)
+        User.objects.create(id = 1)
+        UserProfile.objects.create(
+        type = "author",
+        id = "90a1e228913946f5852483277ecd7e08",
+        displayName = "meilin1",
+        github = "http://github.Meilin-Lyu",
+        host = "127.0.0.1:8000",
+        url = "http://127.0.0.1:8000/author/90a1e228-9139-46f5-8524-83277ecd7e08",
+        follow = "{}",
+        user_id = 1
 
+        )
+
+    
+    def test_friend_request(self):
+        User.objects.create(id = 1)
+        FriendRequest.objects.create(
+            type = "follow",
+            summary ="...",
+            actor = {},
+            object = {}
+        )
+
+    def test_comments(self):
+        User.objects.create(id = 1)
+        Comment.objects.create(
+            type = "comment",
+            author = "Meilin2",
+            post = "http://127.0.0.1:8000/author/90a1e228-9139-46f5-8524-83277ecd7e08/posts/a0d494d8-d315-491c-aec7-05477ddadf6e",
+            
+
+
+
+
+        )
         
+
 
         
        
